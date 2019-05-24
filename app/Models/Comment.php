689f9435a554be5ad0ab71d6e0bf57354ff1f06e
RocketKou <49176565+RocketKou@users.noreply.github.com>
2019-05-24 09:48:47 +0800

    'cmeet-测试问题三'

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,6 +9,11 @@ class Comment extends Model
 
     public $aMeet = '测试问题一：此处是开发者A的修改部分';
 
+    public $cMeet = '
+    测试问题三：
+    此时远端已经是B的最新提交了，如果开发者A修改了没有pull就提交会怎样
+    ';
+
 
     protected $fillable = ['issue_id','name','email','content'];
 
